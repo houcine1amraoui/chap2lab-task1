b4c4a41e5813f56bf002fc4a9faa3f10bb05548e
houcine1amraoui <houcine1amraoui@gmail.com>
2025-02-24 10:18:31 +0100

    auth updated

--- server/server.js
+++ server/server.js
@@ -15,6 +15,17 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
 
+const users = [
+  {
+    username: "mohamed-msila",
+    clearPassword: "mohamed2024",
+  },
+  {
+    username: "amina-msila",
+    clearPassword: "amina2024",
+  },
+];
+
 //
 app.get("/login", function (req, res) {
   res.sendFile(path.resolve(__dirname, "login.html"));
@@ -23,6 +34,15 @@ app.get("/login", function (req, res) {
 app.post("/login", function (req, res) {
   const { username, password } = req.body;
   console.log(username, password);
+  // validation
+  const result = users.find((user) => {
+    return user.username === username && user.password === user.password;
+  });
+  if (result) {
+    return res.send("Welcome");
+  } else {
+    return res.status(200).send("Invalid username or password");
+  }
   res.status(200).send();
 });
 
